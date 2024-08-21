8394f14749671afbd99ff843cb872de91d46d715
szborok <borok.szabolcs@gmail.com>
2024-08-21 21:25:46 +0200

    Describtion added to main.java

--- ToolManager/src/main/java/hu/euroform/Main.java
+++ ToolManager/src/main/java/hu/euroform/Main.java
@@ -7,6 +7,13 @@ import hu.euroform.Models.Matrix;
 import hu.euroform.Simulation.reserveSimulation;
 
 public class Main {
+    //Database wont be coded till the program fully complete for version one.
+    //Custom exception is made/used.
+    //User input test is included in my simulation as written data. Who does not work in manufacturing profession can not do a user imput test, so no reason for that. Sorry.
+    //I have a project which is a hangman game what you can play through terminal, below you can see the Github repo link. 
+    //It even use external library to send back the definition of the guessed word.
+    //https://github.com/szborok/Hangman.git
+
     public static void main(String[] args) {
         // Set up file naming before any logging occurs
         LogManagerConfig.setupFileNaming();
